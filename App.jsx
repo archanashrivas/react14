b7e2fb9f81e67d3989441ba225eb697badb4cac1
Archana shrivas <127471423+archanashrivas@users.noreply.github.com>
2024-09-16 16:10:46 +0530

    react14

---
+++ App.jsx
@@ -0,0 +1,15 @@
+export default function App() {
+  return (
+  <div className="w-full h-screen bg-slate-100  ">
+    <h1 className ="text-3xl text-blue-950">
+    hello i am here </h1> 
+        <button className="bg-blue-500 text-white py-3 px-5 mt-3 rounded-lg  m-2 ml-2  hover:bg-blue-700 ">  
+        
+        send
+      </button> 
+
+  </div>
+  )
+}
+
+
